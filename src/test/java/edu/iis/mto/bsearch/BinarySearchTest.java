2438076e85ea06bace1a45920135783f23f6c422
monikaciezka <223959@edu.p.lodz.pl>
2021-03-15 19:18:56 +0100

    testy_v1: szukany element w 1 elementowej sekwencji

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -1,19 +1,31 @@
 package edu.iis.mto.bsearch;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 class BinarySearchTest {
 
+    int list[];
+    SearchResult searchResult;
+
     @BeforeEach
-    void setUp() throws Exception {}
+    void setUp() throws Exception {
+        searchResult = new SearchResult();
+    }
 
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void singleElmExistsInSeq() {
+        list = new int[]{5};
+        searchResult.setPosition(1);
+        assertThat(BinarySearch.search(5, list), Matchers.equalTo(searchResult));
     }
 
 }
